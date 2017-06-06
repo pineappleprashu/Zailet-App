e94574084ace8513c482f1f153bf0a15135e7e62
Karan <karanbatra31@gmail.com>
2017-06-06 13:51:55 +0530

    initial commit

--- app/src/main/java/com/example/android/zailet/MainActivity.java
+++ app/src/main/java/com/example/android/zailet/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //comment
 
         if(isNetworkAvailable()) {
             Intent intent = new Intent(MainActivity.this, TopicsActivity.class);
